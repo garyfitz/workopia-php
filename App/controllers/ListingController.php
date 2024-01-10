e307418ff0bc3e015748be36141fb071547a42f6
Gary Fitzgerald <garyfitz82@gmail.com>
2024-01-10 17:39:47 +0000

    Added edit listing functionality

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -63,7 +63,7 @@ class ListingController
     // Check if listing exists
     If(!$listing) {
       ErrorController::notFound('Listing not found or does not exist');
-      exit();
+      return;
     }
 
     loadView('listings/show', [
@@ -103,6 +103,7 @@ class ListingController
         'errors' => $errors,
         'listing' => $newListingData
       ]);
+      exit;
     } else {
       // Submit Data
       $fields = [];
@@ -126,6 +127,9 @@ class ListingController
       $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
 
       $this->db->query($query, $newListingData);
+
+      // Set flash message
+      $_SESSION['success_message'] = 'Listing added sucessfully';
 
       redirect('/listings');
     }
@@ -157,5 +161,103 @@ class ListingController
     $_SESSION['success_message'] = 'Listing deleted sucessfully';
     
     redirect('/listings');
+  }
+
+  /**
+  * Show the listing edit form
+  * 
+  * @param array $params
+  * @return void
+  */
+  public function edit($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if listing exists
+    If(!$listing) {
+      ErrorController::notFound('Listing not found or does not exist');
+      return;
+    }
+
+    loadView('listings/edit', [
+      'listing' => $listing
+    ]);
+  }
+
+  /**
+  * Update a listing
+  * 
+  * @param array $params
+  * @return void
+  */
+  public function update($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if listing exists
+    If(!$listing) {
+      ErrorController::notFound('Listing not found or does not exist');
+      return;
+    }
+
+    $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+    $updateValues = [];
+
+    $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+    $updateValues = array_map('sanitize', $updateValues);
+
+    $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+
+    $errors = [];
+
+    foreach($requiredFields as $field){
+      if(empty(($updateValues[$field])) || !Validation::string($updateValues[$field])) {
+        $errors[$field] = ucfirst($field) . ' is required';
+      }
+    }
+
+    if(!empty($errors)) {
+      // Reload view with errors
+      loadView('listings/edit', [
+        'errors' => $errors,
+        'listing' => $listing
+      ]);
+      exit;
+    } else {
+      // Submit Data
+      
+      $updateFields = [];
+      foreach(array_keys($updateValues) as $field) {
+        $updateFields[] = "{$field} = :{$field}";
+      }
+
+      $updateFields = implode(', ', $updateFields);
+
+      $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+      
+      $updateValues['id'] = $id;
+
+      $this->db->query($updateQuery, $updateValues);
+
+      // Set flash message
+      $_SESSION['success_message'] = 'Listing updated sucessfully';
+
+      redirect('/listings/' . $id );
+    }
+
   }
 }
