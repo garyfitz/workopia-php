f6539ab22825a6492811eb131b10bb1efb870e38
Gary Fitzgerald <garyfitz82@gmail.com>
2024-01-10 09:32:13 +0000

    Added spl_autoload for classes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,16 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+
+// Don't need to require these due to autoload
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+spl_autoload_register(function ($class){
+  $path = basePath('Framework/' . $class . '.php');
+  if(file_exists($path)){
+    require $path;
+  }
+});
 
 // Instantiating the Router
 $router =  new Router();
